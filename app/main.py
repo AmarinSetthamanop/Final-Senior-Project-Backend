2c03f7a0b45b3a8f2d35bbf7fff4e2c45f6dfc22
AmarinSetthamanop <Amarin1499900393711Setthamanop@gmail.com>
2025-04-29 18:09:33 +0700

    update commit

---
+++ app/main.py
@@ -0,0 +1,337 @@
+from typing import Union
+from fastapi import FastAPI, UploadFile, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse #สำหรับส่งไฟล์กลับไปเเบบ asynchronously
+from openpyxl import Workbook #library ที่ออกเเบบมาเพื่อทำงานกับ excel โดยเฉพาะ โดยสามารถทำงานต่างๆที่เกี่ยวข้องกับ excel ได้เเทบทุกอย่าง
+from openpyxl.utils import get_column_letter #สำหรับเเปลงตัวเลขที่เเทนตำเเหน่งคอลัมน์ให้กลายเป็นตัวอักษรที่ใช้เเทนคอลัมน์ใน excel
+from openpyxl.styles import NamedStyle, Font, PatternFill, Border, Side #สำหรับตกเเต่ง font ในตารางข้อมูล 
+import pytesseract
+import re
+from app import pre_image #นำเข้าโมดูลจากไฟล์อื่นๆ
+
+
+
+app = FastAPI()
+
+
+origins = [ #รายชื่อเเหล่งที่มาที่ควรได้รับอนุญาตให้ทำการร้องขอข้ามเเหล่งที่มา
+    "https://receipt-ocr-app-8c0a9.web.app",
+    "http://localhost:5173",
+    "http://202.28.34.197:8027",
+    "*"
+]
+
+app.add_middleware( #CORS or Cross-Origin Resource Sharing
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/")
+def testApi():
+    return {
+        "text": "api is runing..."
+    }
+
+
+@app.post("/write/excel")
+async def write_excel(receipt_data: Request):
+
+    file_path = "..\\write-file\\receipt.xlsx" # สำหรับทำงานใน Docker เพราะ Work Dir ชื่อ /app ดังนั้นใช้ .. เพื่อออกจาก dir app
+    # file_path = ".\\write-file\\receipt.xlsx" # สำหรับทำงานในเครื่อง local
+
+    try:
+        
+        receipt_data_json = await receipt_data.json() #สร้าง json
+
+        columns_set = []
+        cell_length = []
+
+        for data_json in receipt_data_json['result']:
+            for key in data_json:
+                columns_set.append(key)
+                cell_length.append(0)
+            break
+
+        columns_set = sorted(columns_set, key=lambda x: int(re.findall(r'\d+', x)[0]))
+        print(columns_set)
+
+
+        file = Workbook() #Workbook คือ object ที่หน้าตาเหมือนกับ Excel สร้างมาเพื่อเป็นตัวเเทนของไฟล์ Excel ที่เราต้องการ อ่าน-เขียน
+        
+        print(file)
+        print(file.worksheets) #ถามถึงเเผ่นงาน
+        print(file.sheetnames) #ถามชื่อเเผ่นงาน
+
+        sheet = file['Sheet'] 
+
+        custom_style = NamedStyle(name='custom style')
+        
+        custom_style.font = Font(name='TH SarabunPSK', size=14, bold=True) #กำหนด font
+        custom_style.fill = PatternFill('solid', fgColor='FFFFCC')
+        custom_style.border = Border(left=Side(border_style='thin', 
+                                            color='B2B2B2'), 
+                                            right=Side(border_style='thin', 
+                                                        color='B2B2B2'), 
+                                                        top=Side(border_style='thin', 
+                                                                color='B2B2B2'),
+                                                                bottom=Side(border_style='thin',
+                                                                            color='B2B2B2')
+                                    )
+  
+        i = 1
+        for data_json in receipt_data_json['result']:
+            j = 1
+            for key in columns_set:
+                sheet.cell(i, j).value = data_json[key]
+                sheet.cell(i, j).style = custom_style
+
+                j += 1
+            i += 1
+
+        for data_json in receipt_data_json['result']:
+            i = 0
+            for key in columns_set:
+                if len(data_json[key]) > cell_length[i]:
+                    cell_length[i] = len(data_json[key])
+                i += 1
+        
+        print(cell_length) #เเสดงความยาวสูงสุดของเเต่ละ cell
+
+        for i in range(len(columns_set)):
+            columns_name = get_column_letter(i + 1) #เเปลงตัวเลขที่เเทนตำเเหน่งของคอลัมน์ไปเป็นตัวอักษรที่เเทนตำเเหน่งของคอลัมน์ที่ใช้ใน Excel โดยฟังก์ชันนี้จะรองรับหลายตัวอักษรด้วยเช่น AA BB
+            sheet.column_dimensions[columns_name].width = (cell_length[i] + 5) #ขยายความกว้างของคอลัมน์
+            
+        
+        file.save(file_path) #บันทึกออกมาเป็นไฟล์ Excel
+
+        return FileResponse(file_path)
+    
+    except Exception:
+        print('Error in this service')
+
+
+
+
+@app.post("/image/tesseract")
+async def extract_receipt_information(file: UploadFile):
+
+    contents = await file.read() #อ่านข้อมูลทั้งหมดจากไฟล์
+    img = pre_image.preprocessing(contents)
+
+
+    result = [] #ประกาศตัวเเปรสำหรับเก็บข้อมูลของใบเสร็จตาม pattern ที่กำหนด
+    current_index = 0
+    company_name = ''
+
+    bigc_pattern = r'^\d+.\d+\s+\d{13}'
+    makro_pattern = r'^\d+\s+\d{13}'
+    lotus_pattern = r'^\d{14}\s+\w+'
+
+    try:
+        text = pytesseract.image_to_string(img, lang='tha+eng', config='--psm 6') #เเปลงรูปภาพใบเสร็จไปเป็น text
+        print(text)
+        
+        text_line = text.split('\n') #เเบ่งบรรทัดตามการขึ้นบรรทัดใหม่ \n
+
+        for i in range(len(text_line)):
+
+            if re.match(bigc_pattern, text_line[i]):
+                print('Hello big c : ', text_line[i])
+
+                company_name = 'บริษัท บิ๊กซี ซูเปอร์เซ็นเตอร์ จำกัด (มหาชน)'
+                current_index = i
+
+                break
+
+            elif re.match(makro_pattern, text_line[i]):
+                print('Hello makro : ', text_line[i])
+
+                company_name = 'บริษัท ซีพี เเอ็กซ์ตร้า จำกัด (มหาชน)'
+                current_index = i
+
+                break
+
+            elif re.match(lotus_pattern, text_line[i]):
+                print('Hello lotus : ', text_line[i])
+
+                company_name = 'บริษัท เอก-ชัย ดีสทริบิวชั่น ซิสเทม จำกัด'
+                current_index = i
+
+                break
+            else:
+                company_name = 'not found'
+
+        if company_name != 'not found':
+
+            #กำหนดชื่อบริษัท
+            result.append({
+                "item1": company_name
+            })
+
+            #กำหนด pattern สำหรับเก็บข้อมูล
+            result.append({
+                "item1": "จำนวน",
+                "item2": "รหัสสินค้า",
+                "item3": "รายการสินค้า",
+                "item4": "หน่วยบรรจุ",
+                "item5": "ราคาต่อหน่วย",
+                "item6": "ส่วนลด บาท",
+                "item7": "รหัสภาษี",
+                "item8": "จำนวนเงิน (รวม VAT)"
+            })
+
+            print('ตำเเหน่งของ array ปัจจุบัน = ', current_index)
+            print(text_line[current_index])
+
+            result = await search_important_data(result, text_line, current_index, bigc_pattern, makro_pattern, lotus_pattern)
+    
+        else:
+            print('รูปภาพไม่ถูกต้อง ต้องเป็น makro bigc lotus เท่านั้น')
+
+            result = []
+
+    except Exception:
+        result = []
+    
+    
+    return {
+        "result": result
+    }
+
+
+
+async def search_important_data(result, text_line, current_index, bigc_pattern, makro_pattern, lotus_pattern):
+    payment_amount = 0 #ประกาศตัวเเปรสำหรับเก็บ ยอดเงินชำระ
+    count = 0
+
+    i = current_index
+
+    while i < len(text_line):
+
+        number = 0 #ประกาศตัวเเปรสำหรับเก็บจำนวนของบรรทัดที่จะบวกไปหาข้อมูลที่เกินของรายการสินค้าในเเถวนั้นๆ
+        over = "" #สำหรับเก็บข้อมูลที่เกินของรายการสินค้าในเเถวนั้นๆออกมา
+        product_name = "" #ประกาศตัวเเปรสำหรับสร้างรายการสินค้าที่มีการ join ข้อมูลใน List
+
+        if count >= 3:
+            print('วนลูปจนถึงเเถวที่ไม่ต้องการ ทำการหยุดลูป โดยที่ count = ', count)
+            break
+
+
+        if re.match(bigc_pattern, text_line[i]):
+
+            words = text_line[i].split() #เเบ่งข้อความตามการเว้นวรรค
+            print(words)
+
+
+            count2 = 1 #สำหรับนับจำนวนของบรรทัดที่จะบวกไปหาข้อมูลที่เกินของรายการสินค้าในเเถวนั้นๆ
+            while True:
+                if text_line[i+count2] == "": #ตรวจสอบว่าบรรทัดต่อๆไปของเเถวนั้นๆมีค่าว่างหรือไม่
+                    count2 += 1 #ถ้ามีค่าว่างที่ให้ count2 บวกไปทีละ 1 เเล้ววนลูปไปเรื่อยๆจนกว่าจะเจอเเถวที่มีข้อมูล
+                else:
+                    number += count2 #ถ้าเจอเเถวมีข้อมูลก็ให้เก็บจำนวนของบรรทัดที่จะบวกไปหาข้อมูลที่เกินของรายการสินค้าในเเถวนั้นๆ เเละหยุดการทำงานของลูป
+                    break
+
+
+            if (not re.match(bigc_pattern, text_line[i+number])) and (not re.match(r'ออกแทนใบกํากับภาษีอย่างย่อ\s+เลขที่\s+\d+', text_line[i+number])):
+                over = text_line[i+number] #นำข้อมูลที่เกินของรายการสินค้าในเเถวนั้นๆออกมา
+                product_name = " ".join(words[2:-3]) + over #นำข้อมูลที่เกินมาต่อเข้ากับรายการสินค้าของตัวมัน
+
+                i += number
+
+            else:
+                print("กรณีที่ไม่มีข้อความเกินจนขึ้นบรรทัดใหม่")
+                product_name = " ".join(words[2:-3]) #เพิ่มรายการสินค้า, การ join หมายความว่านำข้อมูลใน List มารวมกันเเละเเทนที่ช่องที่ต่อกันด้วย " " หรือจะใส่ "-"
+
+
+            result.append({
+                "item1": words[0], #เพิ่มจำนวน
+                "item2": words[1], #เพิ่มรหัสสินค้า
+                "item3": product_name, #เพิ่มรายการสินค้า
+                "item4": "", #เนื่องจากใบเสร็จ big c ไม่มี column สำหรับข้อมูลหน่วยบรรจุ ดังนั้นจึงใส่ค่าว่าง
+                "item5": words[-3], #เพิ่มราคาต่อหน่วย (รวม VAT), การใช้ตัวเลขติดลบในการเข้าถึงสมาชิกของ List จะเป็นการเข้าถึงสมาชิกจากท้ายสุดมา -3 หมายถึงสมาชิกตัวที่สามจากด้านท้ายสุดของ List
+                "item6": words[-2], #เพิ่มส่วนลด บาท, -2 หมายถึงสมาชิกตัวที่ 2 จากด้านท้ายสุดของ List
+                "item7": "", #เนื่องจากใบเสร็จ big c ไม่มี column สำหรับข้อมูล VAT CODE ดังนั้นจึงใส่ค่าว่าง
+                "item8": words[-1] #เพิ่มจำนวนเงิน (รวม VAT), -1 หมายถึงสมาชิกตัวเเรกจากด้านท้ายสุดของ List
+            })
+
+            payment_amount += float(words[-1]) #ทำการหาผลรวมสำหรับ ยอดเงินชำระ
+
+
+        elif re.match(makro_pattern, text_line[i]):
+            
+            words = text_line[i].split() #เเบ่งข้อความตามการเว้นวรรค
+            print(words)
+
+            column4 = words[-4]
+            column3 = words[-5]
+            column4_data = ''
+            column5_data = ''
+
+            if column4.startswith(tuple("0123456789")) == True and column3.isnumeric() == False:
+                column4_data = column4
+                column5_data = " ".join(words[2:-4])
+
+            elif column4.startswith(tuple("0123456789")) == False and column3.isnumeric() == True:
+                column4_data = column3 + '' + column4
+                column5_data = " ".join(words[2:-5])
+            
+            else:
+                column4_data = column4
+                column5_data = " ".join(words[2:-4])
+
+
+            result.append({
+                "item1": words[0], #เพิ่มจำนวน
+                "item2": words[1], #เพิ่มรหัสสินค้า
+                "item3": column5_data, #เพิ่มรายการสินค้า, การ join หมายความว่านำข้อมูลใน List มารวมกันเเละเเทนที่ช่องที่ต่อกันด้วย " " หรือจะใส่ "-"
+                "item4": column4_data, #เพิ่มหน่วยบรรจุ, -4 หมายถึงสมาชิกตัวที่ 4 จากด้านท้ายสุดของ List
+                "item5": words[-3], #เพิ่มราคาต่อหน่วย (รวม VAT), การใช้ตัวเลขติดลบในการเข้าถึงสมาชิกของ List จะเป็นการเข้าถึงสมาชิกจากท้ายสุดมา -3 หมายถึงสมาชิกตัวที่สามจากด้านท้ายสุดของ List
+                "item6": "", #เนื่องจากใบเสร็จ makro ไม่มี column สำหรับข้อมูล ส่วนลด บาท ดังนั้นจึงใส่ค่าว่าง
+                "item7": words[-2], #เพิ่มข้อมูล VAT CODE, -2 หมายถึงสมาชิกตัวที่สองจากด้านท้ายสุดของ List
+                "item8": words[-1] #เพิ่มจำนวนเงิน (รวม VAT), -1 หมายถึงสมาชิกตัวเเรกจากด้านท้ายสุดของ List
+            })
+
+            payment_amount += float(words[-1]) #ทำการหาผลรวมสำหรับ ยอดเงินชำระ
+
+        elif re.match(lotus_pattern, text_line[i]):
+            
+            words = text_line[i].split() #เเบ่งข้อความตามการเว้นวรรค
+            print(words)
+
+            result.append({
+                "item1": words[-4], #เพิ่มจำนวน
+                "item2": words[0], #เพิ่มรหัสสินค้า
+                "item3": " ".join(words[1:-4]), #เพิ่มรายการสินค้า, การ join หมายความว่านำข้อมูลใน List มารวมกันเเละเเทนที่ช่องที่ต่อกันด้วย " " หรือจะใส่ "-"
+                "item4": "", #เพิ่มหน่วยบรรจุ, -4 หมายถึงสมาชิกตัวที่ 4 จากด้านท้ายสุดของ List
+                "item5": words[-3], #เพิ่มราคาต่อหน่วย (รวม VAT), การใช้ตัวเลขติดลบในการเข้าถึงสมาชิกของ List จะเป็นการเข้าถึงสมาชิกจากท้ายสุดมา -3 หมายถึงสมาชิกตัวที่สามจากด้านท้ายสุดของ List
+                "item6": "", #เนื่องจากใบเสร็จ makro ไม่มี column สำหรับข้อมูล ส่วนลด บาท ดังนั้นจึงใส่ค่าว่าง
+                "item7": "", #เพิ่มข้อมูล VAT CODE, -2 หมายถึงสมาชิกตัวที่สองจากด้านท้ายสุดของ List
+                "item8": words[-2] + " บ" #เพิ่มจำนวนเงิน (รวม VAT), -1 หมายถึงสมาชิกตัวเเรกจากด้านท้ายสุดของ List
+            })
+
+            payment_amount += float(words[-2]) #ทำการหาผลรวมสำหรับ ยอดเงินชำระ
+
+        else:
+            count += 1
+
+
+        i += 1
+
+    #เพิ่มข้อมูล ยอดเงินชำระ
+    result.append({
+        "item1": "ยอดเงินชำระ",
+        "item2": "",
+        "item3": "",
+        "item4": "",
+        "item5": "",
+        "item6": "",
+        "item7": "",
+        "item8": "{:.2f}".format(payment_amount)
+    })
+
+    return result
---
+++ app/pre_image.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+from imutils.perspective import four_point_transform #เมื่อใช้ imutils.perspective เราจำเป็นต้องลง scipy ด้วยเนื่องจากเป็นโมดูลที่จำเป็นสำหรับ imutils.perspective ทำการลงด้วย pip install scipy
+
+
+
+def preprocessing(contents):
+
+    nparr = np.fromstring(contents, np.uint8)
+
+    imRGB = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print(imRGB.shape)
+
+    imGray = cv2.cvtColor(imRGB, cv2.COLOR_BGR2GRAY)
+    print(imGray.shape)
+
+    blur = cv2.GaussianBlur(imGray, (3, 3), 0)
+
+    image_path = scan_detection(blur, imRGB)
+
+    img = image_smoothening(image_path)
+
+    return img
+
+    
+    
+
+def scan_detection(blur, imRGB):
+
+    image_path = '..\\uploads\\scan_detection.jpg' # สำหรับทำงานใน Docker เพราะ Work Dir ชื่อ /app ดังนั้นใช้ .. เพื่อออกจาก dir app
+    # image_path = '.\\uploads\\scan_detection.jpg' # สำหรับทำงานในเครื่อง local
+
+    height = imRGB.shape[0]
+    width = imRGB.shape[1]
+
+    contour_ratio = ((width * height) * 0.5)
+
+
+    #สร้าง numpy array ที่เก็บพิกัดของ 4 มุมของรูปภาพต้นฉบับ
+    #ใช้เป็นค่าเริ่มต้นสำหรับการวาดเส้นขอบ ของรูปภาพต้นฉบับ ในกรณีที่ไม่พบรูปทรงที่ต้องการ
+    #โดยเเต่ละมุมจะมีตำเเหน่ง x,y ทั้งหมด 4 มุม
+    
+    document_contour = np.array([[0, 0], [width, 0], [width, height], [0, height]])
+    print(document_contour)
+    
+
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
+    #contour คือเส้นโค้งที่เชื่อมจุดต่อเนื่องทั้งหมด โดยมีสีหรือความเข้มเท่ากัน
+    #contour เป็นเครื่องมือที่มีประโยชน์สำหรับการวิเคราะห์รูปร่าง การตรวจจับเเละการจดจำวัตถุ
+    #contour เเต่ละตัวจะเป็น numpy array ของพิกัด x,y ของจุดขอบเขตของรูปร่าง
+
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0] #ค้นหารูปทรง ซึ่งจะเก็บรูปทรงทั้งหมดที่พบ เป็น List
+    
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+
+    max_area = 0
+    for contour in contours:
+
+        area = cv2.contourArea(contour) #คำนวณพื้นที่ของ contour นั้นๆ
+        
+        if area > contour_ratio:
+
+            peri = cv2.arcLength(contour, True) #หาความยาวเส้นโค้ง
+            approx = cv2.approxPolyDP(contour, 0.015 * peri, True)
+
+            if area > max_area and len(approx) == 4:
+
+                max_area = area
+                document_contour = approx
+
+    # cv2.drawContours(imRGB, [document_contour], -1, (255, 0, 255), 3)
+
+    warped = four_point_transform(imRGB, document_contour.reshape(4, 2))
+    cv2.imwrite(image_path, warped)
+
+    return image_path
+
+
+
+
+def image_smoothening(image_path):
+
+    imRGB = cv2.imread(image_path)
+    height = imRGB.shape[0]
+    width = imRGB.shape[1]
+    
+    if height < 4000 and width < 4000:
+        imRGB = cv2.resize(imRGB, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    
+    imGray = cv2.cvtColor(imRGB, cv2.COLOR_BGR2GRAY)
+
+    thresh = cv2.threshold(imGray, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    thresh = thresh[10:thresh.shape[0] - 10, 10:thresh.shape[1] - 10]
+
+    cv2.imwrite('..//uploads//thresh.jpg', thresh) # สำหรับทำงานใน Docker เพราะ Work Dir ชื่อ /app ดังนั้นใช้ .. เพื่อออกจาก dir app
+    # cv2.imwrite('.//uploads//thresh.jpg', thresh) # สำหรับทำงานในเครื่อง local
+
+    return thresh
